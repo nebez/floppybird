6ca4588b19a236796afef307d0b10ec1fceb37d0
Nebez Briefkani <nebez.briefkani@shopify.com>
2019-09-12 14:41:16 -0400

    Optimize emoji:text ratio

--- README.md
+++ README.md
@@ -1,52 +1,44 @@
-Floppy Bird
-=========
-Initially, I wanted to try recreating Flappy Bird in HTML5, using a canvas and recycling the assets from the old game.
+# [play floppy bird](https://nebez.github.io/floppybird/)
 
-What I did instead is something way more primitive. This project uses nothing but good ol' div's for all the objects and graphics, will scale perfectly on almost any screen (mobile and desktop), is terribly unoptimized, laggy, and isn't nearly as fast as it would've been if I had just used a canvas instead. But here it is!
+If you missed the Flappy Bird hype, here's your chance to try the best vintage knockoff.
 
-Live App
-------------
-Check out a live preview of it over here:  
-http://nebez.github.io/floppybird/
+**Features**
 
-Cool Stuff
----------
-*Some cool things other people have done with the code. Let me know about your projects and I'll link it here*  
-https://wanderingstan.github.io/handybird/ - **[@wanderinstan](https://github.com/wanderingstan)** - Real hand gestures to play Flappy Bird, using doppler effect and microphone.
-http://www.hhcc.com/404 - **[Hill Holiday](http://www.hhcc.com/)** using it for their 404  
-http://heart-work.se/duvchi - Floppy bird, modified, and used as a promotional campaign for an album release  
-https://www.progressivewebflap.com/ - **[@jsonthor](https://twitter.com/jsonthor)** turned Floppy Bird into a PWA!
-https://github.com/rukmal/FlappyLeapBird - **[Rukmal](http://rukmal.me/)** integrated LeapMotion Controller functionality! Check out his website, he's done some cool stuff.  
-http://chrisbeaumont.github.io/floppybird/ - **[@chrisbeaumont](https://github.com/chrisbeaumont)** made an awesome auto-pilot, check it out  
-http://www.lobe.io/flappy-math-saga/- **[@tikwid](https://github.com/tikwid)** made a really cool version designed to teach you times tables. really cool.  
-http://dota2.cyborgmatt.com/flappydota/ - flappy dota, this one is really cool.  
-http://tippy.gochiusa.net/ - Japanese anime inspired floppybird.  
-http://emdigital.ru/wiki — floppybird on **[EyeMedia Instagram Marketing Agency](http://emdigital.ru/)** website. 
-http://labs.aylien.com/flappy-bird/ - **[@mdibaiee/flappy-es](https://github.com/mdibaiee/flappy-es)** - Playing Flappy Bird using Evolution Strategies
+* 🎉 good ol' div's for all the objects and graphics,
+* 🖥 scales perfectly on almost any screen, both mobile and desktop,
+* 💩 unoptimized, laggy, and not nearly as fast as a canvas implementation,
 
-Credits
-------
-**[@aregowe](https://github.com/aregowe)** for optimizing all the assets
+Enjoy.
 
-Notice
-=====
-The assets powering the visual element of the game have all been extracted directly from the Flappy Bird android game. I have only done the coding, not designed the visual elements.  
-I do not own the assets, nor do I have explicit permission to use them from their creator. They are the work and copyright of original creator Dong Nguyen and .GEARS games (http://www.dotgears.com/).  
-I took this Tweet (https://twitter.com/dongatory/status/431060041009856512 / http://i.imgur.com/AcyWyqf.png) by Dong Nguyen, the creator of the game, as an open invitation to reuse the game concept and assets in an open source project. There is no ill intention to steal the game, or claim it as my own. This is merely a recreation for fun.  
-If the copyright holder would like for the assets to be removed, let me know!
+https://nebez.github.io/floppybird/ (or play [**easy mode**](https://nebez.github.io/floppybird/?easy))
 
+### Clones
 
-License
-=====
-Copyright 2014 Nebez Briefkani
+* https://wanderingstan.github.io/handybird/
+    * **[@wanderinstan](https://github.com/wanderingstan)** enables hand gestures to play using doppler effect and a microphone
+* http://www.hhcc.com/404
+    * **[Hill Holiday](http://www.hhcc.com/)** using it for their 404
+* http://heart-work.se/duvchi
+    * promotional campaign for an album release
+* https://www.progressivewebflap.com/
+    * **[@jsonthor](https://twitter.com/jsonthor)** lets you take floppy bird with you as a progressive web app
+* https://github.com/rukmal/FlappyLeapBird
+    * **[Rukmal](http://rukmal.me/)** integrates the LeapMotion Controller
+* http://chrisbeaumont.github.io/floppybird/
+    * **[@chrisbeaumont](https://github.com/chrisbeaumont)** puts the bird on auto-pilot
+* http://www.lobe.io/flappy-math-saga/
+    * **[@tikwid](https://github.com/tikwid)** teaches you math
+* http://dota2.cyborgmatt.com/flappydota/
+    * flappy dota
+* http://labs.aylien.com/flappy-bird/
+    * **[@mdibaiee/flappy-es](https://github.com/mdibaiee/flappy-es)** brings skynet to floppy bird
+* https://emu.edu/gaming-hub/flappy-huxman-game/
+    * university celebrates 100 years by putting President Susan Huxman on a floppy bird body
 
-Licensed under the Apache License, Version 2.0 (the "License");  
-you may not use this file except in compliance with the License.  
-You may obtain a copy of the License at  
-http://www.apache.org/licenses/LICENSE-2.0
+### Notice
 
-Unless required by applicable law or agreed to in writing, software  
-distributed under the License is distributed on an "AS IS" BASIS,  
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
-See the License for the specific language governing permissions and  
-limitations under the License.
+The assets powering the visual element of the game have all been extracted directly from the Flappy Bird android game. I do not own the assets, nor do I have explicit permission to use them from their creator. They are the work and copyright of original creator Dong Nguyen and .GEARS games (http://www.dotgears.com/).
+
+I took this Tweet (https://twitter.com/dongatory/status/431060041009856512 / http://i.imgur.com/AcyWyqf.png) by Dong Nguyen, the creator of the game, as an open invitation to reuse the game concept and assets in an open source project. There is no intention to steal the game, monetize it, or claim it as my own.
+
+If the copyright holder would like for the assets to be removed, please open an issue to start the conversation.
--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-/*
-   Copyright 2014 Nebez Briefkani
-   floppybird - main.js
-
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
-
-       http://www.apache.org/licenses/LICENSE-2.0
-
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
-
 var debugmode = false;
 
 var states = Object.freeze({
@@ -59,12 +42,12 @@ $(document).ready(function() {
       debugmode = true;
    if(window.location.search == "?easy")
       pipeheight = 200;
-   
+
    //get the highscore
    var savedscore = getCookie("highscore");
    if(savedscore != "")
       highscore = parseInt(savedscore);
-   
+
    //start with the splash screen
    showSplash();
 });
@@ -73,7 +56,7 @@ function getCookie(cname)
 {
    var name = cname + "=";
    var ca = document.cookie.split(';');
-   for(var i=0; i<ca.length; i++) 
+   for(var i=0; i<ca.length; i++)
    {
       var c = ca[i].trim();
       if (c.indexOf(name)==0) return c.substring(name.length,c.length);
@@ -92,28 +75,28 @@ function setCookie(cname,cvalue,exdays)
 function showSplash()
 {
    currentstate = states.SplashScreen;
-   
+
    //set the defaults (again)
    velocity = 0;
    position = 180;
    rotation = 0;
    score = 0;
-   
+
    //update the player in preparation for the next game
    $("#player").css({ y: 0, x: 0});
    updatePlayer($("#player"));
-   
+
    soundSwoosh.stop();
    soundSwoosh.play();
-   
+
    //clear out all the pipes if there are any
    $(".pipe").remove();
    pipes = new Array();
-   
+
    //make everything animated again
    $(".animated").css('animation-play-state', 'running');
    $(".animated").css('-webkit-animation-play-state', 'running');
-   
+
    //fade in the splash
    $("#splash").transition({ opacity: 1 }, 2000, 'ease');
 }
@@ -121,14 +104,14 @@ function showSplash()
 function startGame()
 {
    currentstate = states.GameScreen;
-   
+
    //fade out the splash
    $("#splash").stop();
    $("#splash").transition({ opacity: 0 }, 500, 'ease');
-   
+
    //update the big score
    setBigScore();
-   
+
    //debug mode?
    if(debugmode)
    {
@@ -140,7 +123,7 @@ function startGame()
    var updaterate = 1000.0 / 60.0 ; //60 times a second
    loopGameloop = setInterval(gameloop, updaterate);
    loopPipeloop = setInterval(updatePipes, 1400);
-   
+
    //jump from the start!
    playerJump();
 }
@@ -149,33 +132,33 @@ function updatePlayer(player)
 {
    //rotation
    rotation = Math.min((velocity / 10) * 90, 90);
-   
+
    //apply rotation and position
    $(player).css({ rotate: rotation, top: position });
 }
 
 function gameloop() {
    var player = $("#player");
-   
+
    //update the player speed/position
    velocity += gravity;
    position += velocity;
-   
+
    //update the player
    updatePlayer(player);
-   
+
    //create the bounding box
    var box = document.getElementById('player').getBoundingClientRect();
    var origwidth = 34.0;
    var origheight = 24.0;
-   
+
    var boxwidth = origwidth - (Math.sin(Math.abs(rotation) / 90) * 8);
    var boxheight = (origheight + box.height) / 2;
    var boxleft = ((box.width - boxwidth) / 2) + box.left;
    var boxtop = ((box.height - boxheight) / 2) + box.top;
    var boxright = boxleft + boxwidth;
    var boxbottom = boxtop + boxheight;
-   
+
    //if we're in debug mode, draw the bounding box
    if(debugmode)
    {
@@ -185,32 +168,32 @@ function gameloop() {
       boundingbox.css('height', boxheight);
       boundingbox.css('width', boxwidth);
    }
-   
+
    //did we hit the ground?
    if(box.bottom >= $("#land").offset().top)
    {
       playerDead();
       return;
    }
-   
+
    //have they tried to escape through the ceiling? :o
    var ceiling = $("#ceiling");
    if(boxtop <= (ceiling.offset().top + ceiling.height()))
       position = 0;
-   
+
    //we can't go any further without a pipe
    if(pipes[0] == null)
       return;
-   
+
    //determine the bounding box of the next pipes inner area
    var nextpipe = pipes[0];
    var nextpipeupper = nextpipe.children(".pipe_upper");
-   
+
    var pipetop = nextpipeupper.offset().top + nextpipeupper.height();
    var pipeleft = nextpipeupper.offset().left - 2; // for some reason it starts at the inner pipes offset, not the outer pipes.
    var piperight = pipeleft + pipewidth;
    var pipebottom = pipetop + pipeheight;
-   
+
    if(debugmode)
    {
       var boundingbox = $("#pipebox");
@@ -219,7 +202,7 @@ function gameloop() {
       boundingbox.css('height', pipeheight);
       boundingbox.css('width', pipewidth);
    }
-   
+
    //have we gotten inside the pipe yet?
    if(boxright > pipeleft)
    {
@@ -227,7 +210,7 @@ function gameloop() {
       if(boxtop > pipetop && boxbottom < pipebottom)
       {
          //yeah! we're within bounds
-         
+
       }
       else
       {
@@ -236,14 +219,14 @@ function gameloop() {
          return;
       }
    }
-   
-   
+
+
    //have we passed the imminent danger?
    if(boxleft > piperight)
    {
       //yes, remove it
       pipes.splice(0, 1);
-      
+
       //and score a point
       playerScore();
    }
@@ -292,10 +275,10 @@ function setBigScore(erase)
 {
    var elemscore = $("#bigscore");
    elemscore.empty();
-   
+
    if(erase)
       return;
-   
+
    var digits = score.toString().split('');
    for(var i = 0; i < digits.length; i++)
       elemscore.append("<img src='assets/font_big_" + digits[i] + ".png' alt='" + digits[i] + "'>");
@@ -305,7 +288,7 @@ function setSmallScore()
 {
    var elemscore = $("#currentscore");
    elemscore.empty();
-   
+
    var digits = score.toString().split('');
    for(var i = 0; i < digits.length; i++)
       elemscore.append("<img src='assets/font_small_" + digits[i] + ".png' alt='" + digits[i] + "'>");
@@ -315,7 +298,7 @@ function setHighScore()
 {
    var elemscore = $("#highscore");
    elemscore.empty();
-   
+
    var digits = highscore.toString().split('');
    for(var i = 0; i < digits.length; i++)
       elemscore.append("<img src='assets/font_small_" + digits[i] + ".png' alt='" + digits[i] + "'>");
@@ -325,11 +308,11 @@ function setMedal()
 {
    var elemmedal = $("#medal");
    elemmedal.empty();
-   
+
    if(score < 10)
       //signal that no medal has been won
       return false;
-   
+
    if(score >= 10)
       medal = "bronze";
    if(score >= 20)
@@ -338,9 +321,9 @@ function setMedal()
       medal = "gold";
    if(score >= 40)
       medal = "platinum";
-   
+
    elemmedal.append('<img src="assets/medal_' + medal +'.png" alt="' + medal +'">');
-   
+
    //signal that a medal has been won
    return true;
 }
@@ -350,13 +333,13 @@ function playerDead()
    //stop animating everything!
    $(".animated").css('animation-play-state', 'paused');
    $(".animated").css('-webkit-animation-play-state', 'paused');
-   
+
    //drop the bird to the floor
    var playerbottom = $("#player").position().top + $("#player").width(); //we use width because he'll be rotated 90 deg
    var floor = flyArea;
    var movey = Math.max(0, floor - playerbottom);
    $("#player").transition({ y: movey + 'px', rotate: 90}, 1000, 'easeInOutCubic');
-   
+
    //it's time to change states. as of now we're considered ScoreScreen to disable left click/flying
    currentstate = states.ScoreScreen;
 
@@ -387,10 +370,10 @@ function showScore()
 {
    //unhide us
    $("#scoreboard").css("display", "block");
-   
+
    //remove the big score
    setBigScore(true);
-   
+
    //have they beaten their high score?
    if(score > highscore)
    {
@@ -399,16 +382,16 @@ function showScore()
       //save it!
       setCookie("highscore", highscore, 999);
    }
-   
+
    //update the scoreboard
    setSmallScore();
    setHighScore();
    var wonmedal = setMedal();
-   
+
    //SWOOSH!
    soundSwoosh.stop();
    soundSwoosh.play();
-   
+
    //show the scoreboard
    $("#scoreboard").css({ y: '40px', opacity: 0 }); //move it down so we can slide it up
    $("#replay").css({ y: '40px', opacity: 0 });
@@ -417,7 +400,7 @@ function showScore()
       soundSwoosh.stop();
       soundSwoosh.play();
       $("#replay").transition({ y: '0px', opacity: 1}, 600, 'ease');
-      
+
       //also animate in the MEDAL! WOO!
       if(wonmedal)
       {
@@ -425,7 +408,7 @@ function showScore()
          $("#medal").transition({ opacity: 1, scale: 1 }, 1200, 'ease');
       }
    });
-   
+
    //make the replay button clickable
    replayclickable = true;
 }
@@ -439,12 +422,12 @@ $("#replay").click(function() {
    //SWOOSH!
    soundSwoosh.stop();
    soundSwoosh.play();
-   
+
    //fade out the scoreboard
    $("#scoreboard").transition({ y: '-40px', opacity: 0}, 1000, 'ease', function() {
       //when that's done, display us back to nothing
       $("#scoreboard").css("display", "none");
-      
+
       //start the game over!
       showSplash();
    });
@@ -463,7 +446,7 @@ function updatePipes()
 {
    //Do any pipes need removal?
    $(".pipe").filter(function() { return $(this).position().left <= -100; }).remove()
-   
+
    //add a new pipe (top height + bottom height  + pipeheight == flyArea) and put it in our tracker
    var padding = 80;
    var constraint = flyArea - pipeheight - (padding * 2); //double padding (for top and bottom)
